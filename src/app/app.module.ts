281451b1065a09c27e8833bb287060b7d132fc52
Federico Peralta <3rd.federico.peralta@almundo.com>
2022-11-24 23:28:15 -0300

    agregando los componentes para el administrador de fedeDrone Basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToggleMenuComponent } from './toggle-menu/toggle-menu.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToggleMenuComponent,
+    SidebarComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/toggle-menu/toggle-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-menu',
+  templateUrl: './toggle-menu.component.html',
+  styleUrls: ['./toggle-menu.component.css']
+})
+export class ToggleMenuComponent {
+
+}
